eefb84f40e3a896d010183ec8825aff632c1ad2f
Anant Sinha <anantsinha9811@gmail.com>
2018-10-14 13:26:37 +0530

    Parse payload and store in buffer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const url = require('url');
+let {StringDecoder} = require('string_decoder');
+
 let server = http.createServer((req,res)=> {
   //Get the url and parse it
   let parsedUrl = url.parse(req.url);
@@ -9,9 +11,24 @@ let server = http.createServer((req,res)=> {
   let method = req.method;
   //Get the headers
   let headers = req.headers;
+  //Get querystring as an object
+  let queryStringObject = parsedUrl.pathname;
+  console.log(queryStringObject);
   //Get the payload as an object
   console.log('server listening on port 3000');
-res.end();
+
+  //parse payload and store it in buffer
+  let stringDecoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data)=>{
+    buffer+=stringDecoder.write(data);
+  });
+
+  req.on('end', ()=>{
+    buffer+=stringDecoder.end();
+  });
+  
+  res.end();
 });
 
 server.listen(3000);
